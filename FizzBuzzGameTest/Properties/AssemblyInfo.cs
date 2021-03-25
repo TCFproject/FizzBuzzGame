ab95cfb10ea488dcbf3696f79732b8908f3a8a2d
ixiondata974 <thierry.changf@gmail.com>
2021-03-25 10:19:12 +0100

    Ajoutez des fichiers projet.

---
+++ FizzBuzzGameTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FizzBuzzGameTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FizzBuzzGameTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d29ef398-350f-42d4-b365-14f1a6a1dcee")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ FizzBuzzGameTest/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzzGameTest
+{
+    [TestClass]
+    public class FizzBuzzTest
+    {
+        [TestMethod]
+        public void AliceDonneUnNombreMultipleDe3()
+        {
+            int nb = 24;
+
+            String FizzStr = FizzBuzz.BobRepond(nb);
+
+            Assert.AreEqual("Fizz", FizzStr);
+        }
+    }
+}
